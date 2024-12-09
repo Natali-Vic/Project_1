df70b06d9180073a4dc8de7102300bb7a181f024
Natali_Vic <natastafurova@gmail.com>
2024-12-09 23:28:08 +0700

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    //direction: 'vertical',
+    slidesPerView: 3,
+    slidesPerGroup: 4,
+    spaseBetween:10,
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
+new Accordion('.accordion');
+
+console.log('gjhgjhhk');
+console.log(object);
